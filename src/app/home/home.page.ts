2e9d44a234ad56db431b52c4c6b5386491e91e38
Syed Sabbir Sunny <sunny@stockedge.com>
2020-01-22 11:03:27 +0530

    First faceapi

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as faceapi from 'face-api.js'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild("video", { read: ElementRef, static: true }) videoElement: ElementRef
+
+  constructor() {
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/src/app/home/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/src/app/home/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/src/app/home/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/src/app/home/models')
+    ]).then(this.startVideo)
+  }
+
+  playing(e) {
+    console.log(e)
+  }
+
+  startVideo() {
+    navigator.getUserMedia({ video: {} }, stream => {
+      this.videoElement.nativeElement.srcObject = stream
+    }, err => {
+      console.log(err)
+    })
+  }
 
 }
